694d028b9cf1c8f4937dc033cfeb00fc8efb141a
rahmed29 <ryajjn@gmail.com>
2024-07-19 23:39:00 -0700

    Cleaned up

--- streamer.js
+++ streamer.js
@@ -1,40 +1,20 @@
-// I wrote this script when I knew basically nothing about programming. It is very bad but I don't want to touch it because it works and the FFMPEG command is rly long
-
-// set to false if you don't want chroma key'd sub reminder. Sub alert must be in ./ dir and titled template.mp4
-const useSubAlert = true;
-// which clip the sub alert should be on. Defaults to 2
-const subAlertClipNum = 2;
-
 const YTDlpWrap = require("yt-dlp-wrap").default;
+
 // must install ytp-dlp on your system and instantiate node wrapper w/ binary location
 const ytDlpWrap = new YTDlpWrap("/bin/yt-dlp");
-const fs = require("fs");
-var shell = require("shelljs");
+const shell = require("shelljs");
 require("dotenv").config();
 
-function pad(str) {
-  if ((str + "").length < 2) {
-    str = "0" + str;
-  }
-  return str;
-}
-
-let days;
-let start_month;
-let end_month;
-let start_day;
-let end_day;
-let start_year;
-let end_year;
-
-function makeDates(daysBack, monthS, monthE, dateS, dateE, yearS, yearE) {
-  days = daysBack;
-  start_month = monthS;
-  end_month = monthE;
-  start_day = dateS;
-  end_day = dateE;
-  start_year = yearS;
-  end_year = yearE;
+// time delta function
+function makeDates(
+  days,
+  start_month,
+  end_month,
+  start_day,
+  end_day,
+  start_year,
+  end_year
+) {
   if (end_day - days < 1) {
     days = days - start_day;
     if (start_month - 1 == 0) {
@@ -58,7 +38,7 @@ function makeDates(daysBack, monthS, monthE, dateS, dateE, yearS, yearE) {
       start_day = 31;
     }
     if (days >= 0) {
-      makeDates(
+      return makeDates(
         days,
         start_month,
         end_month,
@@ -69,10 +49,10 @@ function makeDates(daysBack, monthS, monthE, dateS, dateE, yearS, yearE) {
       );
     }
   } else {
-    start_day = pad(end_day - days);
-    end_day = pad(new Date().getDate());
-    start_month = pad(start_month);
-    end_month = pad(end_month);
+    start_day = (end_day - days).toString().padStart(2, "0");
+    end_day = new Date().getDate().toString().padStart(2, "0");
+    start_month = start_month.toString().padStart(2, "0");
+    end_month = end_month.toString().padStart(2, "0");
     console.log(
       "Time Range: " +
         start_month +
@@ -87,103 +67,131 @@ function makeDates(daysBack, monthS, monthE, dateS, dateE, yearS, yearE) {
         "/" +
         end_year
     );
+    return [start_month, end_month, start_day, end_day, start_year, end_year];
   }
 }
 
-async function whois(str) {
-  try {
-    const response = await fetch(
-      "https://api.twitch.tv/helix/users?login=" + str,
-      {
-        method: "GET",
-        headers: {
-          Authorization: process.env.AUTH,
-          "Client-Id": process.env.CLIENT,
-        },
-      }
-    );
+// clear downloads folder
+shell.exec("rm -rf ./downloads");
 
-    if (response.ok) {
-      const body = await response.json();
-      console.log(str + "'s broadcaster ID: " + body["data"][0]["id"]);
-      myFunc(body["data"][0]["id"]);
+// process environment variables
+const USE_SUB_ALERT = process.env.USE_SUB_ALERT.toLowerCase() === "true";
+const SUB_ALERT_CLIP_NUM = process.env.SUB_ALERT_CLIP_NUM;
+const SUB_ALERT_FILE = process.env.SUB_ALERT_FILE;
+
+// switch to true after everything is finished
+let finished = false;
+
+// start building FFMPEG command
+let toWrite =
+  USE_SUB_ALERT && SUB_ALERT_CLIP_NUM && SUB_ALERT_FILE
+    ? "\nffmpeg -i ./downloads/input" +
+      SUB_ALERT_CLIP_NUM +
+      ".mp4 -i " +
+      SUB_ALERT_FILE +
+      ' -filter_complex "[1:v]chromakey=0x00D700:0.1:0.2[green];[0:v][green]overlay[outv]" -map "[outv]" -map "0:a" -c:v libx264 -c:a aac -strict experimental ./downloads/input' +
+      SUB_ALERT_CLIP_NUM +
+      "_sub.mp4\n" +
+      "rm ./downloads/input" +
+      SUB_ALERT_CLIP_NUM +
+      ".mp4\n" +
+      "mv ./downloads/input" +
+      SUB_ALERT_CLIP_NUM +
+      "_sub.mp4 ./downloads/input" +
+      SUB_ALERT_CLIP_NUM +
+      ".mp4\n" +
+      "ffmpeg"
+    : "ffmpeg";
+
+// get streamer ID from username
+async function getIdFromName(name, dates) {
+  const response = await fetch(
+    "https://api.twitch.tv/helix/users?login=" + name,
+    {
+      method: "GET",
+      headers: {
+        Authorization: process.env.AUTH,
+        "Client-Id": process.env.CLIENT,
+      },
     }
-  } catch (err) {
-    console.error("Error");
-  }
-}
+  );
 
-async function myFunc(str) {
   try {
-    const response = await fetch(
-      "https://api.twitch.tv/helix/clips?broadcaster_id=" +
-        str +
-        "&started_at=" +
-        start_year +
-        "-" +
-        start_month +
-        "-" +
-        start_day +
-        "T00:00:00Z&ended_at=" +
-        end_year +
-        "-" +
-        end_month +
-        "-" +
-        end_day +
-        "T00:00:00Z",
-      {
-        method: "GET",
-        headers: {
-          Authorization: process.env.AUTH,
-          "Client-Id": process.env.CLIENT,
-        },
-      }
-    );
-
-    if (response.ok) {
-      const body = await response.json();
-      funcTwo(body, (err, output) => {});
-    }
+    const body = await response.json();
+    console.log(name + "'s broadcaster ID: " + body["data"][0]["id"]);
+    getClipsFromId(body["data"][0]["id"], dates);
   } catch (err) {
-    console.error("Error");
+    console.error("TWITCH API RESPONSE NOT OK. ERROR: " + err);
   }
 }
 
-let toWrite = useSubAlert ? `
-ffmpeg -i ./downloads/input${subAlertClipNum || 2}.mp4 -i ./template.mp4 -filter_complex "[1:v]chromakey=0x00D700:0.1:0.2[green];[0:v][green]overlay[outv]" -map "[outv]" -map "0:a" -c:v libx264 -c:a aac -strict experimental ./downloads/input${subAlertClipNum || 2}_sub.mp4\n
-rm ./downloads/input${subAlertClipNum || 2}.mp4\n
-mv ./downloads/input${subAlertClipNum || 2}_sub.mp4 ./downloads/input${subAlertClipNum || 2}.mp4\n
-ffmpeg` : "ffmpeg";
+// get top clips in time frame from broadcaster ID
+async function getClipsFromId(
+  id,
+  [start_month, end_month, start_day, end_day, start_year, end_year]
+) {
+  const response = await fetch(
+    "https://api.twitch.tv/helix/clips?broadcaster_id=" +
+      id +
+      "&started_at=" +
+      start_year +
+      "-" +
+      start_month +
+      "-" +
+      start_day +
+      "T00:00:00Z&ended_at=" +
+      end_year +
+      "-" +
+      end_month +
+      "-" +
+      end_day +
+      "T00:00:00Z",
+    {
+      method: "GET",
+      headers: {
+        Authorization: process.env.AUTH,
+        "Client-Id": process.env.CLIENT,
+      },
+    }
+  );
 
-async function funcTwo(input, callback) {
+  if (response.ok) {
+    const body = await response.json();
+    beginDownloading(body);
+  } else {
+    console.error("TWITCH API RESPONSE NOT OK. ERROR: " + response.status);
+  }
+}
+
+// download clips and keep build FFMPEG command
+async function beginDownloading(input) {
   const vidName = Date.now();
   var cnt = 0;
-  callback(null, input);
   let dat = input["data"];
   for (let i = 0; i < dat.length; i++) {
-    download(dat[i]["url"], i + 1);
-    toWrite = toWrite + " -i ./downloads/input" + (i + 1) + ".mp4";
+    dl(dat[i]["url"], i + 1);
+    toWrite += ` -i ./downloads/input${i + 1}.mp4`;
     cnt += 1;
   }
   console.log("Starting download...");
-  toWrite = toWrite + ' -filter_complex "';
+  toWrite += ' -filter_complex "';
   for (let i = 0; i < cnt; i++) {
-    toWrite = toWrite + "[" + i + ":v]scale=1920x1080[v" + i + "];";
+    toWrite += `[${i}:v]scale=1920x1080[v${i}];`;
   }
   for (let i = 0; i < cnt; i++) {
-    toWrite = toWrite + "[v" + i + "][" + i + ":a]";
+    toWrite += `[v${i}][${i}:a]`;
   }
-  toWrite =
-    toWrite +
+  toWrite +=
     " concat=n=" +
     cnt +
     ':v=1:a=1[outv][outa]" -map "[outv]" -map "[outa]" -c:v libx264 -c:a aac -vsync 2 -strict experimental ./master/' +
     vidName +
     `.mp4\n\nrm -rf ./downloads`;
-  fs.writeFileSync("./merge.sh", toWrite);
+  finished = true;
 }
 
-function download(link, name) {
+// download a single clip
+function dl(link, name) {
   let ytDlpEventEmitter = ytDlpWrap
     .exec([link, "-f", "best", "-o", "./downloads/input" + name + ".mp4"])
     .on("progress", (progress) =>
@@ -199,9 +207,10 @@ function download(link, name) {
     )
     .on("error", (error) => console.error(error));
 
-  console.log("PID: " + ytDlpEventEmitter.ytDlpProcess.pid + " - " + link);
+  console.log(`PID:${ytDlpEventEmitter.ytDlpProcess.pid}-${link}`);
 }
 
+// get user input
 const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -210,25 +219,29 @@ const readline = require("readline").createInterface({
 readline.question(
   "Enter a broadcaster username and how many days back to search separated by a colon:\n",
   (name) => {
+    const strmr = name.split(":")[0];
+    const days = name.split(":")[1];
     dt = new Date();
-    let stmr = name.substring(0, name.indexOf(":"));
-    let days = name.substring(name.indexOf(":") + 1);
-    makeDates(
-      days,
-      dt.getMonth() + 1,
-      dt.getMonth() + 1,
-      dt.getDate(),
-      dt.getDate(),
-      dt.getFullYear(),
-      dt.getFullYear()
+    getIdFromName(
+      strmr,
+      makeDates(
+        days,
+        dt.getMonth() + 1,
+        dt.getMonth() + 1,
+        dt.getDate(),
+        dt.getDate(),
+        dt.getFullYear(),
+        dt.getFullYear()
+      )
     );
-    whois(stmr);
     readline.close();
   }
 );
 
+// execute FFMPEG command on exit
 process.on("exit", () => {
-  if (toWrite != "ffmpeg") {
-    shell.exec("./merge.sh");
+  // only execute command if everything worked
+  if (finished && toWrite != "ffmpeg") {
+    shell.exec(toWrite);
   }
 });
